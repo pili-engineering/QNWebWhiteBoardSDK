043ee4a53d10192a48354d66aa28766071bb050d
Spencer17x <1253478653@qq.com>
2022-05-23 13:20:56 +0800

    fix: base改为.

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ const pkg = require('./package.json');
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   build: {
     outDir: `dist/${pkg.version}`,
   }
