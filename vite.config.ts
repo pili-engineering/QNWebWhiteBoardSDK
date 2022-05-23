10b370ce284d1151801ecd511ada5e5439ec9e71
Spencer17x <1253478653@qq.com>
2022-05-23 11:57:12 +0800

    fix: 修复打包带版本路径

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const pkg = require('./package.json');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    outDir: `dist/${pkg.version}`,
+  }
+});
